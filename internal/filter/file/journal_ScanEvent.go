53bbc14d13763e1f4f82d43c96c79d7c5e4042df
Max V. Buglakov <maxb@tutamail.com>
2022-03-30 15:51:44 +0300

    tune

--- internal/filter/file/journal_ScanEvent.go
+++ internal/filter/file/journal_ScanEvent.go
@@ -89,7 +89,7 @@ const (
 	SOLAR_RADIUS     = 696340000.0
 	EARTH_RADIUS     = 6371.0 * 1000.0
 	LIGHT_SECOND     = 299792.0 * 1000.
-	MIN_RING_OUT_RAD = 20.0 * LIGHT_SECOND
+	MIN_RING_OUT_RAD = 15.0 * LIGHT_SECOND
 )
 
 func formatTemp(temp float64) string {
