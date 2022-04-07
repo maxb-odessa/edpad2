80ab806275315c639b03af5a4f3fce4598a88de9
Max V. Buglakov <maxb@tutamail.com>
2022-04-07 21:44:27 +0300

    yay

---
+++ internal/local/display/display_vars.go
@@ -0,0 +1,24 @@
+package display
+
+// utf pictograms
+const (
+	BLACKHOLE   = "\u269D"
+	STAR4FILLED = "\u2726"
+	STAR4HOLLOW = "\u2727"
+
+	CIRCLEDN = "\u24DD"
+	CIRCLEDY = "\u24E8"
+)
+
+func CIRCLEDNUM(num int) string {
+	if num <= 0 || num > 20 {
+		return string(rune(0x24EA))
+	} else {
+		return string(rune(0x2460 + num))
+	}
+}
+
+func CIRCLEDCHAR(char byte) string {
+	return string(rune(0x24B6 + int(char) - 1))
+
+}
