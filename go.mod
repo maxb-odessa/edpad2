4b4788a3252de4213b13b127ea966aae434dbf03
Max V. Buglakov <maxb@tutamail.com>
2022-04-10 16:41:36 +0300

    kbd

--- go.mod
+++ go.mod
@@ -3,10 +3,11 @@ module edpad2
 go 1.16
 
 require (
+	github.com/bendahl/uinput v1.5.1
 	github.com/danwakefield/fnmatch v0.0.0-20160403171240-cbb64ac3d964
 	github.com/faiface/beep v1.1.0
 	github.com/gotk3/gotk3 v0.6.1
-	github.com/maxb-odessa/gamenode v0.0.1
+	github.com/maxb-odessa/gamenode v0.0.2
 	github.com/maxb-odessa/sconf v0.0.2
 	github.com/maxb-odessa/slog v0.0.1
 	github.com/mitchellh/mapstructure v1.4.3
--- internal/local/display/keypad.go
+++ internal/local/display/keypad.go
@@ -4,6 +4,7 @@ import (
 	"edpad2/internal/router"
 	"fmt"
 
+	uin "github.com/bendahl/uinput"
 	"github.com/gotk3/gotk3/gtk"
 
 	pb "github.com/maxb-odessa/gamenode/pkg/gamenodepb"
@@ -58,6 +59,13 @@ func (h *handler) keypad() error {
 	return nil
 }
 
+func nameToCode(name string) uint32 {
+	if kc, ok := keyCodes[name]; ok {
+		return kc
+	}
+	return uin.KeySpace // TODO default?
+}
+
 func (h *handler) sendKeyEvent(name string, pressed bool) {
 
 	msg := &pb.KbdMsg{
@@ -66,7 +74,7 @@ func (h *handler) sendKeyEvent(name string, pressed bool) {
 			Event: &pb.KbdEvent{
 				Obj: &pb.KbdEvent_Key_{
 					Key: &pb.KbdEvent_Key{
-						Id:      name,
+						Code:    nameToCode(name),
 						Pressed: pressed,
 					},
 				},
@@ -78,4 +86,80 @@ func (h *handler) sendKeyEvent(name string, pressed bool) {
 		Dst:  router.NetKeyboard,
 		Data: msg,
 	}
+}
+
+var keyCodes = map[string]uint32{
+	"esc":       uin.KeyEsc,
+	"escape":    uin.KeyEsc,
+	" ":         uin.KeySpace,
+	"space":     uin.KeySpace,
+	"enter":     uin.KeyEnter,
+	"tab":       uin.KeyTab,
+	"bs":        uin.KeyBackspace,
+	"backspace": uin.KeyBackspace,
+
+	"0": uin.Key0,
+	"1": uin.Key1,
+	"2": uin.Key2,
+	"3": uin.Key3,
+	"4": uin.Key4,
+	"5": uin.Key5,
+	"6": uin.Key6,
+	"7": uin.Key7,
+	"8": uin.Key8,
+	"9": uin.Key9,
+
+	"left":  uin.KeyLeft,
+	"right": uin.KeyRight,
+	"up":    uin.KeyUp,
+	"down":  uin.KeyDown,
+
+	"kp0": uin.KeyKp1,
+	"kp1": uin.KeyKp1,
+	"kp2": uin.KeyKp1,
+	"kp3": uin.KeyKp1,
+	"kp4": uin.KeyKp1,
+	"kp5": uin.KeyKp1,
+	"kp6": uin.KeyKp1,
+	"kp7": uin.KeyKp1,
+	"kp8": uin.KeyKp1,
+	"kp9": uin.KeyKp1,
+	"kp-": uin.KeyKpminus,
+	"kp+": uin.KeyKpplus,
+	"kp*": uin.KeyKpasterisk,
+	"kp/": uin.KeyKpslash,
+
+	"q": uin.KeyQ,
+	"w": uin.KeyW,
+	"e": uin.KeyE,
+	"r": uin.KeyR,
+	"t": uin.KeyT,
+	"y": uin.KeyY,
+	"u": uin.KeyU,
+	"i": uin.KeyI,
+	"o": uin.KeyO,
+	"p": uin.KeyP,
+	"[": uin.KeyLeftbrace,
+	"]": uin.KeyRightbrace,
+	"a": uin.KeyA,
+	"s": uin.KeyS,
+	"d": uin.KeyD,
+	"f": uin.KeyF,
+	"g": uin.KeyG,
+	"h": uin.KeyH,
+	"j": uin.KeyJ,
+	"k": uin.KeyK,
+	"l": uin.KeyL,
+	";": uin.KeySemicolon,
+	"'": uin.KeyApostrophe,
+	"z": uin.KeyZ,
+	"x": uin.KeyX,
+	"c": uin.KeyC,
+	"v": uin.KeyV,
+	"b": uin.KeyB,
+	"n": uin.KeyN,
+	"m": uin.KeyM,
+	",": uin.KeyComma,
+	".": uin.KeyDot,
+	"/": uin.KeySlash,
 }
